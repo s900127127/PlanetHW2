21740570a74c79e31e1e7c0ddbbf3530620e598d
s900127127 <s900127127@gmail.com>
2017-01-26 00:51:24 +0800

    20170126FirstVer

---
+++ PlanetEditor/Assets/Scripts/PlanetEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlanetEditor : MonoBehaviour {
+
+    // Add a menu item named "CreatePlanet" to PlanetPlugin in the menu bar.
+    [MenuItem("PlanetPlugin/CreatePlanet")]
+    static void CreatePlanet()
+    {
+        GameObject planet = new GameObject() { name = "planet" };
+        planet.AddComponent<Planet>().GetComponent<Planet>().Initialize();//Initialize the Planet
+    }
+}
